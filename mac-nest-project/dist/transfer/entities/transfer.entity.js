8523f519d183a1a9c9b40c11c43adb1e9b50e52c
UGoo <ugoo@UGoos-MacBook-Pro.local>
2025-02-10 23:17:36 +0100

    Stop tracking node_modules

--- mac-nest-project/dist/transfer/entities/transfer.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Transfer = void 0;
-class Transfer {
-}
-exports.Transfer = Transfer;
-//# sourceMappingURL=transfer.entity.js.map
